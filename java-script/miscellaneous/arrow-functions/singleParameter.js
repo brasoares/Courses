79a8aae4e051faa40b0caaba20c42da40ecd2e66
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 10:40:25 -0300

    Create singleParameter.js
    Single parameter.

---
+++ java-script/miscellaneous/arrow-functions/singleParameter.js
@@ -0,0 +1,11 @@
+// If there is only one parameter for the function: just leave out the parenthesis, if necessary (preference, readability and so on might influence in such a decision):
+
+// Traditional:
+function isPositive(number) {
+  return number >= 0
+}
+
+// Single parameter way with an arrow function:
+const isPositive = number => {
+  return number >= 0
+}
