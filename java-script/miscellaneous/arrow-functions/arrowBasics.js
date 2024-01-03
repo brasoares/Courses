9583cc7f84a417eb9649d514acf46f763cb7849d
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 10:37:14 -0300

    Create arrowBasics.js
    The study of arrow functions begin!

---
+++ java-script/miscellaneous/arrow-functions/arrowBasics.js
@@ -0,0 +1,18 @@
+// This file not necessarily will run and work, the idea is the study of the concepts, for now:
+
+// The following is the most classical way of declaring and using a function
+function sum (a, b) {
+  return a + b
+}
+
+// Ahead we have an arrow function, where the 'function' keyword will not be necessary:
+// The arrow will go:
+const sum (a, b) => {
+  return a + b
+}
+
+// Now, ahead, we have 'sum' as an arrow function
+
+/*
+As per GPT: 'Arrow functions are a concise way to write anonymous functions in JavaScript. They were introduced in ECMAScript 6 (ES6) and provide a more compact syntax compared to traditional function expressions. Arrow functions are particularly useful for short, simple functions and also introduce a more predictable behavior for the this keyword.'
+*/
