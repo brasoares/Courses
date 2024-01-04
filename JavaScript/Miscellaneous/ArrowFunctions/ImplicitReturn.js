68a0e6826737c6f98d48a3a3c46accaed086c772
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 21:03:40 -0300

    Create ImplicitReturn.js
    Resourceful for future reference

---
+++ JavaScript/Miscellaneous/ArrowFunctions/ImplicitReturn.js
@@ -0,0 +1,7 @@
+// General manner
+function randomNumber(){
+  return Math.random()
+}
+
+// Simplicity if possible
+const randomNumber = () => Math.random() // as randomNumber has no params, a void set of parenthesis is used
