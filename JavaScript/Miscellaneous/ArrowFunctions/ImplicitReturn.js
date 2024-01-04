1e6e4ce6c78b70a925d555adc71ed72f5c794acf
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 21:20:16 -0300

    Update ImplicitReturn.js
    Credits given

--- JavaScript/Miscellaneous/ArrowFunctions/ImplicitReturn.js
+++ JavaScript/Miscellaneous/ArrowFunctions/ImplicitReturn.js
@@ -5,3 +5,5 @@ function randomNumber(){
 
 // Simplicity if possible
 const randomNumber = () => Math.random() // as randomNumber has no params, a void set of parenthesis is used
+
+// As seen on https://blog.webdevsimplified.com/2020-09/arrow-functions/
