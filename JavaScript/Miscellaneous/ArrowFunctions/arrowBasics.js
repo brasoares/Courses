ef07b8d4195a97cf3de46b05e98145e22a8dd75e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 21:19:55 -0300

    Update arrowBasics.js
    Credits given

--- JavaScript/Miscellaneous/ArrowFunctions/arrowBasics.js
+++ JavaScript/Miscellaneous/ArrowFunctions/arrowBasics.js
@@ -16,3 +16,5 @@ const sum (a, b) => {
 /*
 As per GPT: 'Arrow functions are a concise way to write anonymous functions in JavaScript. They were introduced in ECMAScript 6 (ES6) and provide a more compact syntax compared to traditional function expressions. Arrow functions are particularly useful for short, simple functions and also introduce a more predictable behavior for the this keyword.'
 */
+
+// As seen on https://blog.webdevsimplified.com/2020-09/arrow-functions/
