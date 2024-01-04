c088581a7ae99f43bffd91e1d400d9c666af83b5
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-03 21:23:32 -0300

    Create anonymousArrowFunctions.js
    Resource & learning material and annotations.

---
+++ JavaScript/Miscellaneous/ArrowFunctions/anonymousArrowFunctions.js
@@ -0,0 +1,11 @@
+// Classical approach with function declaration
+document.addEventListener("click", function () {
+  console.log("Click")
+})
+
+// No need for explicit use of the keyword (function)
+document.addEventListener("click", () => {
+  console.log("Click")
+})
+
+Credits: https://blog.webdevsimplified.com/2020-09/arrow-functions/
