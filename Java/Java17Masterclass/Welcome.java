a733f8c581e4a34c2a712f344d51a0aefe8b6e5f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-05 23:28:59 -0300

    Update Welcome.java
    Code correction to deliver the average.

--- Java/Java17Masterclass/Welcome.java
+++ Java/Java17Masterclass/Welcome.java
@@ -1,8 +1,6 @@
 // This is an IntelliJ's Community Edition walkthrough code.
 // As I have studied it to learn it does not start exactly like this in their turorial, it is here.
-
 import java.util.Arrays;
-import java.util.List;
 
 class Welcome {
     public static void main(String[] args) {
@@ -15,6 +13,6 @@ class Welcome {
         for (int value : values) {
             result += value;
         }
-        return result;
+        return result / values.length; // Divide the sum by the number of elements
     }
 }
