9204397574b512b2532d447c69b94706f7f83460
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-08 01:07:39 -0300

    Create Dollars.java
    For syntax reviewing!

---
+++ JavaScript/Miscellaneous/JavaInOneHour/Dollars.java
@@ -0,0 +1,8 @@
+public class Dollars{
+    public static void main(String[] args) {
+        System.out.println("Learning the basics.");
+
+        // Array declarations.
+        int[] dollars = {1000000, 1008520, 9050307};
+    }
+}
