510326db3e9a94e5be48c7079b2d0951cd238752
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-08 17:50:34 -0300

    Create ArrayDeclarations.java
    This code is for training the declarations of arrays.

---
+++ Java/JavaInOneHour/ArrayDeclarations.java
@@ -0,0 +1,11 @@
+public class ArrayDeclarations {
+    public static void main(String[] args) {
+        int[] age = new int[10];
+        String[] names = new String[10];
+        boolean[] saoBrasileiras = {true, false, true};
+        /*
+        * Not possible to do:
+        * saoBrasileiras[5] = false; >> because it has been limited above by 3 elements "0, 1, 2"!
+         */
+    }
+}
