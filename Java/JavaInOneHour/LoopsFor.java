ec69cac7a86321288fdc20f4d636cfe94c4c21d7
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:28:31 -0300

    Update LoopsFor.java
    Code updated to the use of var keyword, itself!

--- Java/JavaInOneHour/LoopsFor.java
+++ Java/JavaInOneHour/LoopsFor.java
@@ -1,7 +1,7 @@
 public class LoopsFor {
     public static void main(String[] args){
         int age = 999;
-        for(int i = 0; i < age; i++) {
+        for(var i = 0; i < age; i++) {
             System.out.println("Keep learning!!! Success!!!");
         }
     }
