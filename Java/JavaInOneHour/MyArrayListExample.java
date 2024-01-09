873aa5582a57a9b5ce52ce0049a3ececb9fe2fe9
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:13:09 -0300

    Add files via upload
    Code for example and learning purposes. If more good purposes I found, great!

---
+++ Java/JavaInOneHour/MyArrayListExample.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class MyArrayListExample {
+  // ArrayList: Class to help represent a dynamic list
+  public static void main(String[] args) {
+    // It's necessary to tell what is going to be stored (type)
+    // "Integer", the class is requested in this case (native from Java language, not being aprimitive type, but a class, as the case of String).
+    ArrayList < Integer > age = new ArrayList < Integer > ();
+    // We can manipulate the data inside via methods:
+    age.add(22);
+    age.add(33);
+    age.add(44);
+    age.remove(0);
+    age.get(1);
+    age.size();
+    // Gather a length from normal circumstances:
+    int[] age1 = new int[10];
+    age1.length;
+
+  }
+}
