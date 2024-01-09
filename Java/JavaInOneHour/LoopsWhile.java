fc7e586531ba7dd5b19ad424d932254a84df654d
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:47:56 -0300

    Update LoopsWhile.java
    Code improved to print a dot at the end!

--- Java/JavaInOneHour/LoopsWhile.java
+++ Java/JavaInOneHour/LoopsWhile.java
@@ -1,10 +1,16 @@
 public class LoopsWhile {
     public static void main(String[] args) {
         var golden = 0;
-        while(golden <= 999) {
-            System.out.printf("%d, ", golden);
-            golden++;
+        while (golden <= 999) {
+            System.out.printf("%d", golden);
+            
+            if (golden < 999) {
+                System.out.prin(",");
+            } else {
+                System.out.print(".");
+            }
         }
-    System.out.println(".");
+        
+        golden++;
     }
 }
