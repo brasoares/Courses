eaef2962ff63f564333761821324ca789bb19055
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:44:48 -0300

    Update LoopsWhile.java
    Corrected to print as high as 999 (1000 total).

--- Java/JavaInOneHour/LoopsWhile.java
+++ Java/JavaInOneHour/LoopsWhile.java
@@ -1,7 +1,7 @@
 public class LoopsWhile {
     public static void main(String[] args) {
         var golden = 0;
-        while(golden < 999) {
+        while(golden <= 999) {
             System.out.printf("%d, ", golden);
             golden++;
         }
