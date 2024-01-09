33be60599dfa7e05fe27ef4a7d3d29372f7d3491
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:38:05 -0300

    Update LoopsWhile.java
    Corrected the use of the while.

--- Java/JavaInOneHour/LoopsWhile.java
+++ Java/JavaInOneHour/LoopsWhile.java
@@ -1,7 +1,7 @@
 public class LoopsWhile {
     public static void main(String[] args) {
         var golden = 0;
-        while(golden = 0 < 999) {
+        while(golden = 0) {
             System.out.println("%d, ");
         }
     System.out.println(".");
