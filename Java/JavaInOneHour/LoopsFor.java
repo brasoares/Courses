56ef220c390085aa97a6dc0a40ceedb70ae94aa0
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:22:25 -0300

    Add files via upload
    An example of how to use for loops in Java!

---
+++ Java/JavaInOneHour/LoopsFor.java
@@ -0,0 +1,8 @@
+public class LoopsFor {
+    public static void main(String[] args){
+        int age = 999;
+        for(i = 0; i < age; i++) {
+            System.out.println("Keep learning!!! Success!!!");
+        }
+    }
+}
