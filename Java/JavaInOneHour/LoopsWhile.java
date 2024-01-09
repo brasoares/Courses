ea65dd5416b51069871168d770716c7dc6377bae
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:43:39 -0300

    Update LoopsWhile.java
    printf formatting corrected.

--- Java/JavaInOneHour/LoopsWhile.java
+++ Java/JavaInOneHour/LoopsWhile.java
@@ -2,7 +2,7 @@ public class LoopsWhile {
     public static void main(String[] args) {
         var golden = 0;
         while(golden < 999) {
-            System.out.println("%d, ", golden);
+            System.out.printf("%d, ", golden);
             golden++;
         }
     System.out.println(".");
