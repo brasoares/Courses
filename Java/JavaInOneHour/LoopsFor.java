5b47b02c065ddb3ed07257be6f9223007dbdc4d7
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:27:32 -0300

    Update LoopsFor.java
    Correction for var declaration.

--- Java/JavaInOneHour/LoopsFor.java
+++ Java/JavaInOneHour/LoopsFor.java
@@ -1,7 +1,7 @@
 public class LoopsFor {
     public static void main(String[] args){
         int age = 999;
-        for(i = 0; i < age; i++) {
+        for(int i = 0; i < age; i++) {
             System.out.println("Keep learning!!! Success!!!");
         }
     }
