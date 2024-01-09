644a1d8cc338577880897e8dba805ea55cccb05f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-09 20:33:40 -0300

    Add files via upload
    While example!

---
+++ Java/JavaInOneHour/LoopsWhile.java
@@ -0,0 +1,9 @@
+public class LoopsWhile {
+    public static void main(String[] args) {
+        var golden = 0;
+        while(var i = 0 < 999) {
+            System.out.println("%d, ");
+        }
+    System.out.println(".");
+    }
+}
