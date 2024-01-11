21ecf2c4728bc6e4d9da8246776c682d773ffa6e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-11 17:32:55 -0300

    Update ConstructorsAre.java
    Constructor!

--- Java/JavaInOneHour/OOP/ConstructorsAre.java
+++ Java/JavaInOneHour/OOP/ConstructorsAre.java
@@ -10,9 +10,7 @@ public class ConstructorsAre {
 
     // A constructor is a special method with the same name as the class.
     // It is called automatically every time a new instance of this class is created.
-    // The access modifier 'public' here allows the constructor to be called from outside the class.
-    // If the constructor is made private, it can only be called within the same class.
-    private ConstructorsAre(String name) {
+    public ConstructorsAre(String name) {
         this.name = name;
     }
 }
@@ -20,5 +18,10 @@ public class ConstructorsAre {
 // To create a new instance of ConstructorsAre and call the constructor,
 // use the following syntax: new ConstructorsAre("exampleName");
 
-// Note: The constructor doesn't necessarily need to be public;
-// its visibility depends on the use case. If it's private, it can only be called within the same class.
+// Note: The constructor doesn't necessarily need to be public; its visibility depends on the use case.
+
+class Person {
+    void createConstructor() {
+        ConstructorsAre myConstructorsAre = new ConstructorsAre("Mary");
+    }
+}
