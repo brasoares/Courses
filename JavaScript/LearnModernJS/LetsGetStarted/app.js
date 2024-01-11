06042647280c7a32f67c7887d5b7740dfe2eeeae
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-10 21:13:11 -0300

    Update app.js
    Yeah!

--- JavaScript/LearnModernJS/LetsGetStarted/app.js
+++ JavaScript/LearnModernJS/LetsGetStarted/app.js
@@ -1,4 +1,6 @@
 // Important to notice: JS is case sensitive!
+// Also, the use of semicolons provide better readability and avoid errors as it tells the compiler the statement has ended
+// So, although JavaScript does not require semicolons at the end of every statement, it’s a good practice to include them for better readability and to avoid potential errors.
 
 var userName = "brasoares"
 
