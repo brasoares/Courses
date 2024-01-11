7557e9d5ded74d56c2468f42d1ab1962be486987
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-11 16:06:20 -0300

    Update ClassesAre.java
    Better commenting

--- Java/JavaInOneHour/OOP/ClassesAre.java
+++ Java/JavaInOneHour/OOP/ClassesAre.java
@@ -1,22 +1,18 @@
 public class ClassesAre {
 
     String name; // This attribute 'name' holds the state information for an instance of the ClassesAre class.
-    static String name2; // This one can be referenced by the static main as it is static && also, in the other methods, this one can be referenced!
+    static String name2; // This static variable 'name2' can be referenced by static methods and is associated with the class itself.
 
     public static void main(String[] args) { // static means it belongs to the class, not the object instances
         // The main method is the entry point for the Java program.
         // It can be used to demonstrate the functionality of the ClassesAre class.
-        /*
-        * String cannot be called here, as it would present an error such as:
-        * "Non-static field 'name' cannot be referenced from a static context" in IntelliJ.
-        * It is due to the fact that this static belongs to the class ClassesAre itself, not an instance.
-        */
+        // Example usage: ClassesAre instance = new ClassesAre(); instance.declaresName();
     }
 
     // The declaresName method sets the value of the 'name' attribute to "Light."
     void declaresName() {
         System.out.println(name2);
-        ClassesAre.main(); // Also possible to call Main directly without creating a class object for the Main class.
+        ClassesAre.main(); // Also possible to call the main method directly on the ClassesAre class.
         name = "Light";
     }
 
