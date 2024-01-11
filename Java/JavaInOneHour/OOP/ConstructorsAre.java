b8faceaf7d0794dd4e3fa351ca77d8f0016a4a2e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-11 16:18:33 -0300

    Update ConstructorsAre.java

--- Java/JavaInOneHour/OOP/ConstructorsAre.java
+++ Java/JavaInOneHour/OOP/ConstructorsAre.java
@@ -1,1 +1,19 @@
+public class ConstructorsAre {
 
+    String name;
+
+    static String name2;
+
+    public static void main(String[] args) {
+        System.out.println(name2);
+    }
+
+    // A constructor is a special method with the same name as the class.
+    // It is called automatically every time a new instance of this class is created.
+    public ConstructorsAre(String name) {
+        this.name = name;
+    }
+}
+
+// To create a new instance of ConstructorsAre and call the constructor,
+// use the following syntax: new ConstructorsAre("exampleName");
