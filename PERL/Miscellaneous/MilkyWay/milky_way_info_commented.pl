079d981a3cd1891a033d8472057bfacb9233b04a
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-12 01:08:29 -0300

    Update milky_way_info_commented.pl
    Update to the lesson!

--- PERL/Miscellaneous/MilkyWay/milky_way_info_commented.pl
+++ PERL/Miscellaneous/MilkyWay/milky_way_info_commented.pl
@@ -5,8 +5,29 @@
 # File Name: milky_way_info_commented.pl
 
 # Use strict and warnings to catch common coding errors
-use strict;
-use warnings;
+use strict; // Enforces strict variable declarations and other coding standards
+
+# The 'strict' pragma enforces a stricter set of rules in Perl.
+# It helps catch common mistakes and coding errors, such as using
+# undeclared variables, misspelled variable names, and more.
+
+# 'use strict;' is usually placed at the beginning of Perl scripts
+# to enhance code quality and prevent the use of potentially
+# problematic coding practices.
+
+# Using 'strict' is considered good practice in Perl programming.
+
+use warnings; // Enables additional warnings during script execution
+
+# The 'warnings' pragma enables additional warnings during the execution of the script.
+# It helps identify potential issues and provides more information about possible problems
+# in the code.
+
+# Warnings can catch things like uninitialized variables, deprecated features,
+# and other situations that might lead to unexpected behavior.
+
+# 'use warnings;' is often used in conjunction with 'strict' to make Perl scripts
+# more robust and to catch potential problems early in the development process.
 
 # Information about the Milky Way
 my $milky_way_name = "Milky Way";
