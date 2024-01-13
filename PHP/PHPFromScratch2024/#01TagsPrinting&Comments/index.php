7f4bd6bc1c24be05c9eb3de026fe28d60146c02e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-12 21:06:31 -0300

    Create index.php
    Initial code!

---
+++ PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://cdn.tailwindcss.com"></script> /* This is actually not the ideal manner, but with <link> tags. I am doing this to reinforce the learning experience by practice in correction! */
+</head>
