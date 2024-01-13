65816f0620a8313367d7c4eff278f025b3dc90e4
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-12 21:10:12 -0300

    Update index.php
    Code correction!

--- PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
+++ PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
@@ -4,5 +4,5 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="https://cdn.tailwindcss.com"></script> /* This is actually not the ideal manner, but with <link> tags. I am doing this to reinforce the learning experience by practice in correction! */
+	<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
