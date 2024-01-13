6c6b7230326bce60129f7061519d0a8fc54c3ee4
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-12 22:47:24 -0300

    Update index.php
    More code and PHP open and close tags.

--- PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
+++ PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,5 +9,21 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+	<title>Learn PHP From Scratch</title>
 </head>
+
+<body class="bg-gray-100">
+	<header class="bg-blue-500 text-white p-4">
+		<div class="container mx-auto">
+			<h1 class="text-3x1 font-semibold">Learn PHP From Scratch</h1>
+		</div>
+	</header>
+	<div class="container mx-auto p-4 mt-4">
+		<div class="bg-white rounded-lg shadow-md p-6">
+			<h2 class="text-2x1 font-semibold mb-4">Welcome To The Course</h2>
+			<p>In this course, I am learning the fundamentals of PHP!</p>
+		</div>
+	</div>
+</body>
+
 </html>
