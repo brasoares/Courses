69b31d3000ab17f5f3fcedf0922a53d01cfcdca4
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-12 22:51:07 -0300

    Update index.php
    Code correction with 'echo'!

--- PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
+++ PHP/PHPFromScratch2024/#01TagsPrinting&Comments/index.php
@@ -9,7 +9,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-	<title><?php Learn PHP From Scratch ?></title>
+	<title><?php echo 'Learn PHP From Scratch' ?></title>
 </head>
 
 <body class="bg-gray-100">
