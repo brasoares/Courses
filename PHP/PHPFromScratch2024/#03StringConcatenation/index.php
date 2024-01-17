e1e3c78c66234e31f5104adaf1e4c549f52ffa05
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-17 00:05:45 -0300

    Update index.php

--- PHP/PHPFromScratch2024/#03StringConcatenation/index.php
+++ PHP/PHPFromScratch2024/#03StringConcatenation/index.php
@@ -13,6 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 		<title><?php echo 'Learn PHP From Scratch'; ?></title>
+		<p><?php echo $phraseFor ?></p>
 	</head>
 
 	<body class="bg-gray-100">
