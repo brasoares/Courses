54b673eedcb636464936035c77eccef8fb03052f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-18 00:39:48 -0300

    Update index.js

--- Libraries/React/TheCompleteGuide(Incl.ReactRouter&Redux)/#001ReactJSvsVanillaJS/Vanilla/index.js
+++ Libraries/React/TheCompleteGuide(Incl.ReactRouter&Redux)/#001ReactJSvsVanillaJS/Vanilla/index.js
@@ -1,1 +1,14 @@
-
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze!",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect ())",
+    "Dynamic rendering"
+  ]
+]
