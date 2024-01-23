5e1407b3655dc2adaac07c3be2b8e6b16248ca5d
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-22 23:29:17 -0300

    Update ex012.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex012.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex012.py
@@ -1,1 +1,10 @@
-
+'''
+Ask for two numbers. If
+the first one is larger
+than the second, display
+the second number first
+and then the first
+number, otherwise show
+the first number first and
+then the second.
+'''
