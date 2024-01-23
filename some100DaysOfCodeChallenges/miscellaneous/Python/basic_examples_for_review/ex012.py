64a708253ca9f692402c0acb8d6df358f31d80e7
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-22 23:30:28 -0300

    Update ex012.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex012.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex012.py
@@ -8,3 +8,6 @@ number, otherwise show
 the first number first and
 then the second.
 '''
+
+num1 = float(input("Please, provide the first number: "))
+num2 = float(input("Please, provide the second number: "))
