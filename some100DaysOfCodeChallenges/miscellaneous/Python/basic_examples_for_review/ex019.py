3ad37c6f43b2b8845038cf882dcda10ff02a2ace
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 03:05:15 -0300

    Update ex019.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex019.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex019.py
@@ -1,1 +1,6 @@
-
+'''
+Ask the user to enter 1, 2 or 3. If they enter a 1, display
+the message “Thank you”, if they enter a 2, display
+“Well done”, if they enter a 3, display “Correct”. If
+they enter anything else, display “Error message”.
+'''
