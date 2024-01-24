4b38e7cd41b2ebd502131909582dd3d0dacf17d6
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 03:07:06 -0300

    Update ex019.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex019.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex019.py
@@ -4,3 +4,14 @@ the message “Thank you”, if they enter a 2, display
 “Well done”, if they enter a 3, display “Correct”. If
 they enter anything else, display “Error message”.
 '''
+
+num = int(input("Enter 1, 2 or 3: "))
+
+if num == 1:
+  print("Thank you")
+elif num == 2:
+  print("Well done")
+elif num == 3:
+  print("Correct")
+else:
+  print("Error message")
