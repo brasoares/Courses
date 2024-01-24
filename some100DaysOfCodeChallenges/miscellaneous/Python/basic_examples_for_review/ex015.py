0f19a92f9230b0a1a0a482f6ab2b540b601c82b2
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:21:46 -0300

    Update ex015.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex015.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex015.py
@@ -4,3 +4,11 @@ If they enter “red”, “RED” or
 otherwise display the message
 “I don’t like [colour], I prefer red”.'''
 
+colour = input("Enter your favourite colour: ")
+
+entered_colour = colour.lower()
+
+if entered_colour == 'red':
+  print("I like red, too! :)")
+else:
+  print("I prefer red!") # Bit of a change, here.
