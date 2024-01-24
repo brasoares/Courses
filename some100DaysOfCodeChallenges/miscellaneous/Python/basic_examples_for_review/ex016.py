e8277a08e670486c11432c584df124f762a08e59
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:43:58 -0300

    Update ex016.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex016.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex016.py
@@ -10,7 +10,7 @@ is_raining = input("Is it raining? ").lower()
 
 if is_raining == 'yes':
   is_windy = input("Is it windy? ").lower()
-  if is_windy = 'yes':
+  if is_windy == 'yes':
     print("It is too windy for an umbrella!")
   else:
     print("Take an umbrella!")
