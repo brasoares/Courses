2cb5029755979844c5c25347afe1e64020c0188a
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:54:31 -0300

    Update ex017.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex017.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex017.py
@@ -3,7 +3,7 @@ Ask the user’s age. If they
 are 18 or over, display the
 message “You can vote”, if
 they are aged 17, display the
-message “You can learn to
+message “You can learn how to
 drive”, if they are 16, display
 the message “You can buy a
 lottery ticket”, if they are
