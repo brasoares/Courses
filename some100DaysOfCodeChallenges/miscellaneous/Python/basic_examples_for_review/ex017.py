832432d5bb13eb5e38dae5ee85c682809c59bf4c
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:49:12 -0300

    Update ex017.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex017.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex017.py
@@ -1,1 +1,17 @@
+'''
+Ask the user’s age. If they
+are 18 or over, display the
+message “You can vote”, if
+they are aged 17, display the
+message “You can learn to
+drive”, if they are 16, display
+the message “You can buy a
+lottery ticket”, if they are
+under 16, display the
+message “You can go Trick-
+or-Treating”.
+'''
+age = int(input("How old are you? "))
 
+if age >= 18:
+  print("You can vote")
