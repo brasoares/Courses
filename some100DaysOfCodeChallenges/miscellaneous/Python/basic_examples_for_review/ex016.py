f080c2089e661428b44650b4191a134284520c79
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:40:32 -0300

    Update ex016.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex016.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex016.py
@@ -9,7 +9,7 @@ to the first question, display the answer “Enjoy your day”.
 is_raining = input("Is it raining? ").lower()
 
 if is_raining == 'yes':
-  answer = input("Is it windy? ")
+  is_windy = input("Is it windy? ").lower()
   answer1 = answer.lower()
   if answer1 = 'yes':
     print("It is too windy for an umbrella!")
