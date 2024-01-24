75d00c27dd8fee14217c25816472d3b634d61250
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:04:09 -0300

    Create ex013.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex013.py
@@ -0,0 +1,17 @@
+'''
+Ask the user to enter a
+number that is under
+20. If they enter a
+number that is 20 or
+more, display the
+message “Too high”,
+otherwise display
+“Thank you”.
+'''
+
+num = int(input("Enter a number under 20"))
+
+if num < 20:
+    print("Thank you")
+elif num => 20:
+    print("Too high")
