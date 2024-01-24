bdf57d3b61ecdec67ee3f4252f75f7e0c5fc39a0
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:13:14 -0300

    Update ex014.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex014.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex014.py
@@ -1,8 +1,16 @@
-'''Ask the user to enter a
+'''
+Ask the user to enter a
 number between 10 and 20
 (inclusive). If they enter a
 number within this range,
 display the message “Thank
 you”, otherwise display the
 message “Incorrect
-answer”.'''
+answer”.
+'''
+num = int(input("Enter a number between (inclusive) 10 and 20: "))
+
+if num > 9 and num < 21:
+  print("Thank you!")
+else:
+  print("Incorrect answer.")
