f38f64fa8a037832361cdff3e0005de1a926ed2b
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:58:11 -0300

    Update ex018.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex018.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex018.py
@@ -1,1 +1,6 @@
-
+'''
+Ask the user to enter a number. If it is under 10,
+display the message “Too low”, if their number is
+between 10 and 20, display “Correct”, otherwise
+display “Too high”.
+'''
