44c1153727d908cd2205bf1fd5164cc562c48232
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:17:00 -0300

    Update ex015.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex015.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex015.py
@@ -1,1 +1,6 @@
+'''Ask the user to enter their favourite colour. 
+If they enter “red”, “RED” or
+“Red” display the message “I like red too”, 
+otherwise display the message
+“I don’t like [colour], I prefer red”.'''
 
