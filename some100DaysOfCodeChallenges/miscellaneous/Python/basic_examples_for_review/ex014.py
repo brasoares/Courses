6ca4ea16da9414423b74246ea0948bc3aeea56b1
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-24 02:11:38 -0300

    Create ex014.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/basic_examples_for_review/ex014.py
@@ -0,0 +1,8 @@
+'''Ask the user to enter a
+number between 10 and 20
+(inclusive). If they enter a
+number within this range,
+display the message “Thank
+you”, otherwise display the
+message “Incorrect
+answer”.'''
