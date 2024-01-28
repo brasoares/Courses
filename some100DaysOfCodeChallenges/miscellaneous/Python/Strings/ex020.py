0cf0c0d4ad6c6a0f0804a15d3fedaaaf6afa976c
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-27 23:22:07 -0300

    Update ex020.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex020.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex020.py
@@ -4,3 +4,6 @@ their first name and
 then display the
 length of their name.
 '''
+
+first_name = input("Please, provide your first name: ")
+print(f"Your name is {first_name}, and it contains the length of {} letters.")
