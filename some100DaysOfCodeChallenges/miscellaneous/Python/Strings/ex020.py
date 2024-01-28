14bdb74e38cd581022325e10c9a04e87e6f22211
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-27 23:24:18 -0300

    Update ex020.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex020.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex020.py
@@ -6,4 +6,4 @@ length of their name.
 '''
 
 name = input("Please, provide your first name: ")
-print(f"Your name is {first_name}, and it contains the length of {len(name)} letters.")
+print(f"Your name is {name}, and it contains the length of {len(name)} letters.")
