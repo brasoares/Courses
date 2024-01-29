6541824867866369c951fb3bc59f0320e668840d
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-28 23:20:25 -0300

    Update ex021.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex021.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex021.py
@@ -3,3 +3,10 @@ Ask the user to enter their first name and then ask them to
 enter their surname. Join them together with a space between
 and display the name and the length of whole name.
 '''
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+
+full_name = name + " " + surname
+
+print(full_name)
