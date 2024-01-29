9bed7e8a8c7ef779e8fcd403c626aa8c6116b37f
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-28 23:25:01 -0300

    Update ex022.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex022.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex022.py
@@ -1,1 +1,10 @@
+'''
+Ask the user to enter their first name and surname in lower
+case. Change the case to title case and join them together.
+Display the finished result.
+'''
+
+print("All in lower case, please:")
+name = input("Enter your name")
+surname = input("Enter your surname")
 
