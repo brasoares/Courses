b55dad918d8fdc08d4e55cc8776755d285c66ca5
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-28 23:38:40 -0300

    Update ex023.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex023.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex023.py
@@ -29,3 +29,9 @@ end = int(input("Please, enter an ending number: "))
 
 # Display just that section of the text
 print(f"The requested section is: {nursery_rhyme[start:end]}.")
+
+'''
+"{nursery_rhyme[start:end]}" is a string slicing operation in Python.
+For example, if nursery_rhyme is "Mary had a little lamb" and start is 18 and end is 22,
+then "{nursery_rhyme[start:end]}" will return: "lamb."
+'''
