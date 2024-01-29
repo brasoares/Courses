e16125668318d9d3c6dca4a555f9e7d2cdc77274
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-28 23:46:42 -0300

    Create ex025.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex025.py
@@ -0,0 +1,16 @@
+'''
+Ask the user to enter their first name. If the length
+of their first name is under five characters, ask
+them to enter their surname and join them
+together (without a space) and display the name
+in upper case. If the length of the first name is five
+or more characters, display their first name in
+lower case.
+'''
+
+name = input("Enter your name: ")
+
+if name.length() < 5:
+  surname = input("Enter your surname: ")
+  joined_full_name = name + surname
+  print(joined_full_name)
