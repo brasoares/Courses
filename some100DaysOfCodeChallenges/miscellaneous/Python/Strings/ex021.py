3106793cdd8732d19a43cc2f5edae024fa7305aa
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-28 23:19:01 -0300

    Update ex021.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex021.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex021.py
@@ -1,1 +1,5 @@
-
+'''
+Ask the user to enter their first name and then ask them to
+enter their surname. Join them together with a space between
+and display the name and the length of whole name.
+'''
