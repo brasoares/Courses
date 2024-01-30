311df96c75849d230a2c0e021d47f91bf957069c
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-29 21:49:35 -0300

    Update ex026.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex026.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex026.py
@@ -8,13 +8,16 @@ aadvarkway. Create a program that will ask the
 user to enter a word and change it into Pig Latin.
 Make sure the new word is displayed in lower case.
 '''
-Book's answer, actually:
 
 word = input("Please enter a word: ")
 first = word[0]
 length = len(word)
-rest = word(1:length)
-if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+rest = word(1: length)
+if first != "a"
+and first != "e"
+and first != "i"
+and first != "o"
+and first != "u":
     newword = rest + first + "ay"
 else
     newword = word + "way"
