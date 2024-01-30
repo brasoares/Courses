996bee4d3c31cacf1483f2000643a943703861c1
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-29 21:39:49 -0300

    Update ex026.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex026.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/ex026.py
@@ -1,1 +1,10 @@
-
+'''
+Pig Latin takes the first consonant of a word,
+moves it to the end of the word and adds on an
+“ay”. If a word begins with a vowel you just add
+“way” to the end. For example, pig becomes igpay,
+banana becomes ananabay, and aadvark becomes
+aadvarkway. Create a program that will ask the
+user to enter a word and change it into Pig Latin.
+Make sure the new word is displayed in lower case.
+'''
