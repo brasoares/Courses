83f2a0e9ee399b0e0f29115cd29ad04704d20b00
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-31 22:40:18 -0300

    Update data.py
    CodeBeautify!

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/data.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/data.py
@@ -1,9 +1,9 @@
 data = [
-	["Symbol", "How to type this into a Python string"],
-	["\"", "\\\""], # Escape double quote with a backlash
-	["'", "\\'"], # Escape single quote with a backlash
-	["\\", "\\\\"], # Escape backlash with another backlash
+    ["Symbol", "How to type this into a Python string"],
+    ['"', '\\"'],  # Escape double quote with a backlash
+    ["'", "\\'"],  # Escape single quote with a backlash
+    ["\\", "\\\\"],  # Escape backlash with another backlash
 ]
 
 for row in data:
-	print("\t".join(row)) # tabs for column separtion
+    print("\t".join(row))  # tabs for column separtion
