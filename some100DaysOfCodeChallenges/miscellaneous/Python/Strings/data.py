9f490386fb45bb71c6f25e05621e98447e66d99a
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-31 22:38:30 -0300

    Update data.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Strings/data.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Strings/data.py
@@ -1,6 +1,6 @@
 data = [
 	["Symbol", "How to type this into a Python string"],
 	["\"", "\\\""], # Escape double quote with a backlash
-	[]
-	[]
+	["'", "\\'"], # Escape single quote with a backlash
+	["\\", "\\\\"], # Escape backlash with another backlash
 ]
