744067d23d822a615c1067ce558384c2823d616e
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-31 22:43:02 -0300

    Update ex028.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex028.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex028.py
@@ -2,3 +2,9 @@
 Update program 027 so that it will display the answer to
 two decimal places.
 '''
+
+floatNum = float(input("Enter a number with several decimal places: "))
+
+doubleFloatNum = floatNum * 2
+
+print(f"The result is: {doubleFloatNum}.")
