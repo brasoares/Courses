3b860e2714a1e47bb0f2ea377c1e1b863c3098d1
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-01 21:52:28 -0300

    Update ex029.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex029.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex029.py
@@ -3,3 +3,8 @@ Ask the user to enter an integer that is over 500. Work
 out the square root of that number and display it to two
 decimal places.
 '''
+
+import math
+
+While integer <= 500:
+  integer = int(input("Enter an integer higher than 500: "))
