87cd1326482f901fc9e22dee3cbf73a9c503d0ff
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-02 22:30:35 -0300

    Update ex031.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex031.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex031.py
@@ -3,3 +3,8 @@ Ask the user to enter the radius of a circle
 (measurement from the centre point to the edge). Work
 out the area of the circle (π*radius²).
 '''
+
+radius = float(input("Please, provide the radius of a circle: "))
+area = circle_radius = math.pi * pow(radius, 2)
+
+print(f"Area: {area}.")
