5272f80e7f2b32198a8de5decb67ae524f5ef014
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-02 22:41:34 -0300

    Update ex032.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex032.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex032.py
@@ -4,11 +4,11 @@ and work out the total volume (circle
 area*depth) rounded to three decimal
 places.
 '''
-
+from decimal import Decimal, ROUND_HALF_UP
 import math
 
-radius = float(input("Enter the cylinder's radius: "))
-depth = float(input("Enter the cylinder's depth: "))
+radius = Decimal(input("Enter the cylinder's radius: "))
+depth = Decimal(input("Enter the cylinder's depth: "))
 
 circle_area = math.pi * pow(radius, 2)
 volume = circle_area * depth
