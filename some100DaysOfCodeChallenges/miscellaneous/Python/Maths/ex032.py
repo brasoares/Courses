9cc54959e1a6557656391f98da30b0ce185db0bb
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-02 22:38:40 -0300

    Update ex032.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex032.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex032.py
@@ -10,3 +10,7 @@ import math
 radius = float(input("Enter the cylinder's radius: "))
 depth = float(input("Enter the cylinder's depth: "))
 
+circle_area = math.pi * pow(radius, 2)
+volume = circle_area * depth
+
+print(f"Total volume: {volume:.3f}")
