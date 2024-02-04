4f6570d800a74a18e76b7bf70a59af17148fff13
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-03 21:40:57 -0300

    Create ex033.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex033.py
@@ -0,0 +1,10 @@
+'''
+Ask the user to enter two numbers.
+Use whole number division to divide
+the first number by the second and
+also work out the remainder and
+display the answer in a user-friendly
+way (e.g. if they enter 7 and 2 display
+“7 divided by 2 is 3 with 1
+remaining”).
+'''
