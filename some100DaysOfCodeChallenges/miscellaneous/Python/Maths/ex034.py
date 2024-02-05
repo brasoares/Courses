9c2b9a7b712ed4a45f88b7a53b543e3197633e5d
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-04 23:36:48 -0300

    Create ex034.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex034.py
@@ -0,0 +1,12 @@
+'''
+Display the following message:
+
+1) Square 
+2) Triangle 
+
+Enter a number: 
+
+If the user enters 1, then it should ask them for the length of one of its sides and display
+the area. If they select 2, it should ask for the base and height of the triangle and display
+the are. If they type in anything else, it should give them a suitable error message.
+'''
