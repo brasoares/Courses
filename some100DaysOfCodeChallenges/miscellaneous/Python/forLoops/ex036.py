e23a24e02f7f801e2bf1165ae82b2925174315f9
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-06 22:15:29 -0300

    Update ex036.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex036.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex036.py
@@ -1,2 +1,10 @@
 '''
+Alter program 035 so that it will ask the user to enter their
+name and a number and then display their name that
+number of times.
 '''
+
+users_name = input("Please, enter you first name: ")
+times = int(input("Enter a number: "))
+for i in range (0, times+1):
+  print(users_name)
