4a11265c35af3e7aa0a6556b4fe8240ac1aeb460
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-08 23:54:53 -0300

    Update ex038.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex038.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex038.py
@@ -1,1 +1,16 @@
+'''
+Change program
+037 to also ask for a
+number. Display
+their name (one
+letter at a time on
+each line) and
+repeat this for the
+number of times
+they entered.
+'''
+name = input("Please, enter your name: ")
+times = int(input("Enter the number of time you want to see your name letter by letter: "))
 
+for char in name:
+  print(char)
