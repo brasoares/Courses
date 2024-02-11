817be0ff9a74c1ccee68abc8bde6eafc081cea2f
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-11 20:53:07 -0300

    Update ex036.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex036.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/Maths/ex036.py
@@ -1,2 +1,8 @@
+'''
+Alter program 035 so that it will ask the user to enter their
+name and a number and then display their name that
+number of times.
+'''
+
 name = input("Enter your name: ")
 print(name * 3)
