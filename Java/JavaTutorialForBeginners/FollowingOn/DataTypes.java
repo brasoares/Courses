d8b0adbbf1946811b9c8fe78c4f140765c4b8f00
Henoc Soares Freire <henocsoares@hotmail.com.br>
2024-02-12 07:44:39 -0300

    Updates

---
+++ Java/JavaTutorialForBeginners/FollowingOn/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes 
+{
+    public static void main(Strings a[])
+    {
+     float marks = 9.999; 
+     byte  trezentos = 333;
+    }
+}
---
+++ Java/JavaTutorialForBeginners/FollowingOn/VarStudy.java
@@ -0,0 +1,9 @@
+public class VarStudy {
+    public static void main(String a[]) 
+    {
+        int num1 = 3;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
