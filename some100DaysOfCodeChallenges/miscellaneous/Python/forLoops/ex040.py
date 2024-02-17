11c138be97cd26c39fad14568171cda5ca885cc9
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-16 23:51:02 -0300

    Update ex040.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex040.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex040.py
@@ -3,7 +3,9 @@ Ask for a number below 50 and then count down from
 50 to that number, making sure you show the number
 they entered in the output.
 '''
-num = int(input("Please, enter a number that is less than 50 and starts from 0: "))
+num = 50
 
-while num >= 49:
-  num = int(input("Enter a number in the requested range: "))
+while num > 49 and num < 0:
+  num = int(input("Please, enter a number that is less than 50 and starts from 0: "))
+
+
