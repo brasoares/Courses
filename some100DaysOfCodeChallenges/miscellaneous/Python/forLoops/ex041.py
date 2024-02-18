2ced7a35f840c85d84685de8f9cb29da73e1a5be
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-17 23:42:54 -0300

    Update ex041.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex041.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex041.py
@@ -1,4 +1,4 @@
-'''
+"""
 Ask the user to enter their
 name and a number. If the
 number is less than 10, then
@@ -6,14 +6,14 @@ display their name that
 number of times; otherwise
 display the message “Too
 high” three times.
-'''
+"""
 
 name = input("Enter your name: ")
 
 while n < 1:
-  n = int(input("Now, please, enter a number: "))
+    n = int(input("Now, please, enter a number: "))
 
 if n < 10:
-        print(name * n)
+    print(name * n)
 else:
-  print("Too high" * 3)
+    print("Too high" * 3)
