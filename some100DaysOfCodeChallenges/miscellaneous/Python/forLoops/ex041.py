ee9406e9d5405863604dd5972b43cc8dea39de45
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-17 23:40:30 -0300

    Create ex041.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex041.py
@@ -0,0 +1,17 @@
+'''
+Ask the user to enter their
+name and a number. If the
+number is less than 10, then
+display their name that
+number of times; otherwise
+display the message “Too
+high” three times.
+'''
+
+name = input("Enter your name: ")
+
+while n < 1:
+  n = int(input("Now, please, enter a number: ")
+
+if n < 10:
+        print(name * n)
