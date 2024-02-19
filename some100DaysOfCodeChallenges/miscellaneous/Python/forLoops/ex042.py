043fa880c01f5b9bfee3571b47ceb1042aee57d2
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-18 23:46:18 -0300

    Update ex042.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex042.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex042.py
@@ -6,3 +6,20 @@ number to the total. If they do not want it included,
 don’t add it to the total. After they have entered all five
 numbers, display the total.
 '''
+
+total = 0
+n = 0
+
+for i in range(total):
+  if i < 4:
+    n = int(input("Enter a number: "))
+    total += n
+  elif:
+    i == 4:
+    choice = input("Do you want to include the last input in the sum? (Y or N): ")
+    if choice.lower() == 'y'
+      total += n
+      print(f"{total}")
+else:
+  print(total)
+    
