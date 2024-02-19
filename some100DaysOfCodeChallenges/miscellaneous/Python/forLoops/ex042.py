fdebef1294b64a6ec37e8951b7a995d1d4bbd768
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-18 23:38:22 -0300

    Update ex042.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex042.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex042.py
@@ -1,2 +1,8 @@
 '''
+Set a variable called total to 0. Ask the user to enter
+five numbers and after each input ask them if they
+want that number included. If they do, then add the
+number to the total. If they do not want it included,
+don’t add it to the total. After they have entered all five
+numbers, display the total.
 '''
