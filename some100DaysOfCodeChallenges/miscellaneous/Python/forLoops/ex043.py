831b5d7752090f62a69b9a7d29f66d9302126173
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-20 18:13:43 -0300

    Update ex043.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex043.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex043.py
@@ -1,1 +1,6 @@
-
+'''
+Ask which direction the user wants to count (up or down). If they select up, then ask
+them for the top number and then count from 1 to that number. If they select down, ask
+them to enter a number below 20 and then count down from 20 to that number. If they
+entered something other than up or down, display the message “I don’t understand”.
+'''
