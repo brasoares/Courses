b281156a0435a25eecf0b550a351500e21b63b93
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-21 21:13:08 -0300

    Update ex043.py
    Beautified!

--- some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex043.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex043.py
@@ -1,19 +1,19 @@
-'''
+"""
 Ask which direction the user wants to count (up or down). If they select up, then ask
 them for the top number and then count from 1 to that number. If they select down, ask
 them to enter a number below 20 and then count down from 20 to that number. If they
 entered something other than up or down, display the message “I don’t understand”.
-'''
+"""
 
 choice = input("Do you want to count up or down? ").lower()
 
 if choice == "up":
-  top_number = int(input("Enter the top number: "))
-  for i in range(1, top_number + 1):
-    print i;
+    top_number = int(input("Enter the top number: "))
+    for i in range(1, top_number + 1):
+        print(i)
 elif choice == "down":
-  number_below_20 = int(input("Enter a number below 20: "))
-  for i in range(20, number_below_20 - 1, -1):
-    print(i)
+    number_below_20 = int(input("Enter a number below 20: "))
+    for i in range(20, number_below_20 - 1, -1):
+        print(i)
 else:
-  print("I don't understand")
+    print("I don't understand")
