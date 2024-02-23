e0daf849235a3e92d98ce5bb60c3ffeea159db4b
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-22 21:42:43 -0300

    Create ex044.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/forLoops/ex044.py
@@ -0,0 +1,5 @@
+'''
+Ask how many people the user wants to invite to a party. If they enter a number below
+10, ask for the names and after each name display “[name] has been invited”. If they
+enter a number which is 10 or higher, display the message “Too many people”.
+'''
