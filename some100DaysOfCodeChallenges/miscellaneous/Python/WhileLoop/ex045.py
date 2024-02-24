edc33d9738435d485deb1864bcedc1f0caab91c3
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-23 23:46:45 -0300

    Update ex045.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoop/ex045.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoop/ex045.py
@@ -4,3 +4,9 @@ the user to input a number. Add that number to the total and
 print the message “The total is… [total]”. Stop the loop when
 the total is over 50.
 '''
+
+number = 0
+
+while number <= 50:
+  number = int(input("Input a positive number: "))
+  print(f"The total is... [{number}]")
