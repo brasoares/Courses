5248f79d1bee4d70490fcd06df6427795caa14ab
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-23 23:41:21 -0300

    Create ex045.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoop/ex045.py
@@ -0,0 +1,6 @@
+'''
+Set the total to 0 to start with. While the total is 50 or less, ask
+the user to input a number. Add that number to the total and
+print the message “The total is… [total]”. Stop the loop when
+the total is over 50.
+'''
