6e6327ff0a0456f8301971f122c247734bd2b9c3
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-25 23:32:03 -0300

    Update ex047.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex047.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex047.py
@@ -1,4 +1,4 @@
-'''
+"""
 Ask the user to enter a
 number and then enter
 another number. Add these
@@ -11,31 +11,31 @@ adding numbers until they
 do not answer “y”. Once the
 loop has stopped, display
 the total.
-'''
+"""
 
 n1 = int(input("Enter an integer: "))
 
 # While loop to ensure the entered value is an integer
 while True:
-  try:
-    n2 = int(input("Enter a second integer: "))
-    break # Exit the loop if conversion to integer is successful
-  except ValueError:
-    print("Invalid input! Please enter a valid integer.")
+    try:
+        n2 = int(input("Enter a second integer: "))
+        break  # Exit the loop if conversion to integer is successful
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
 
 total = n1 + n2
 
 choice = input("Do you wish to sum a new number? (Y/N) ").strip().lower()
-while choice != 'n':
-  if choice == 'y':
-    try:
-      n = int(input("Enter an integer: "))
-      total += n
-    except ValueError:
-      print("Invalid input! Please enter a valid integer.")
-  else:
-    print("Invalid input! Please enter a valid integer.")
+while choice != "n":
+    if choice == "y":
+        try:
+            n = int(input("Enter an integer: "))
+            total += n
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
+    else:
+        print("Invalid input! Please enter a valid integer.")
 
-  choice = input("Do you wish to sum a new number? (Y/N) ").strip().lower()
+    choice = input("Do you wish to sum a new number? (Y/N) ").strip().lower()
 
-print("Total:",total)
+print("Total:", total)
