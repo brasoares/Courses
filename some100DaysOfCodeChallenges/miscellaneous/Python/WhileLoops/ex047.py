cd978662d391e4225da07186b473d216fb8d5782
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-25 22:57:35 -0300

    Update ex047.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex047.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex047.py
@@ -1,2 +1,14 @@
 '''
+Ask the user to enter a
+number and then enter
+another number. Add these
+two numbers together and
+then ask if they want to add
+another number. If they
+enter “y", ask them to enter
+another number and keep
+adding numbers until they
+do not answer “y”. Once the
+loop has stopped, display
+the total.
 '''
