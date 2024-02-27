154c2843db5b4c7db99960605b393fcac6f5ee7f
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-26 23:38:38 -0300

    Create ex048.py

---
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex048.py
@@ -0,0 +1,9 @@
+'''
+Ask for the name of somebody the user wants to invite
+to a party. After this, display the message “[name] has
+now been invited” and add 1 to the count. Then ask if
+they want to invite somebody else. Keep repeating this
+until they no longer want to invite anyone else to the
+party and then display how many people they have
+coming to the party.
+'''
