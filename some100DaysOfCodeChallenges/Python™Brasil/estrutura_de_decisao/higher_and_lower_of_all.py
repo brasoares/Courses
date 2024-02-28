f769a977373f6dcc3b6ec96d8a7a10edfa38cd7c
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-28 19:11:12 -0300

    Update higher_and_lower_of_all.py

--- some100DaysOfCodeChallenges/Python™Brasil/estrutura_de_decisao/higher_and_lower_of_all.py
+++ some100DaysOfCodeChallenges/Python™Brasil/estrutura_de_decisao/higher_and_lower_of_all.py
@@ -1,4 +1,4 @@
-"""Make a program that reads three numbers and shows the largest and smallest of them"""
+"""Make a program that reads three numbers and shows the largest and smallest ones"""
 
 n1 = float(input("Enter the 1ˢᵗ #: "))
 n2 = float(input("Enter the 2ⁿᵈ #: "))
