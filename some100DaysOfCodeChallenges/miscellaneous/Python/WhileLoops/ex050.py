eed8d456e410bd5897f289b9162b7a7b863d6597
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-28 23:48:20 -0300

    Update ex050.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex050.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex050.py
@@ -9,3 +9,11 @@ this until they enter a value that is
 between 10 and 20 and then display the
 message “Thank you”.
 '''
+
+while True:
+  n = int(input("Enter a number between 10 and 20: "))
+  if n > 20:
+    print("Too high")
+  elif n < 20:
+    print("Too low")
+print("Thank you")
