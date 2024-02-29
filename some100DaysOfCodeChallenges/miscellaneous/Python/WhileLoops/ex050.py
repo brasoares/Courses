ad930dee361f142e3fcff2f47fdd1a598f8e289e
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-28 23:46:37 -0300

    Update ex050.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex050.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex050.py
@@ -1,2 +1,11 @@
 '''
+Ask the user to enter a number between
+10 and 20. If they enter a value under 10,
+display the message “Too low” and ask
+them to try again. If they enter a value
+above 20, display the message “Too high”
+and ask them to try again. Keep repeating
+this until they enter a value that is
+between 10 and 20 and then display the
+message “Thank you”.
 '''
