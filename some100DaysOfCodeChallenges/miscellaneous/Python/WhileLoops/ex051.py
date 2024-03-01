31a2f0c6c6ceddd32a745730e10302a003f311da
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-29 22:06:10 -0300

    Update ex051.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex051.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex051.py
@@ -1,4 +1,4 @@
-'''
+"""
 Using the song “10 green bottles”, display the lines “There are [num] green bottles
 hanging on the wall, [num] green bottles hanging on the wall, and if 1 green bottle
 should accidentally fall”. Then ask the question “how many green bottles will be
@@ -6,20 +6,22 @@ hanging on the wall?” If the user answers correctly, display the message “Th
 [num] green bottles hanging on the wall”. If they answer incorrectly, display the
 message “No, try again” until they get it right. When the number of green bottles gets
 down to 0, display the message “There are no more green bottles hanging on the wall”.
-'''
+"""
 
 num = 10
 
-print(f"There are {num} green bottles hanging on the wall, {num} green bottles hanging on the wall, and if 1 green bottle should accidentally fall")
+print(
+    f"There are {num} green bottles hanging on the wall, {num} green bottles hanging on the wall, and if 1 green bottle should accidentally fall"
+)
 
 while True:
-  how_many = int(input("How many green bottles will be hanging on the wall? "))
-  num -= 1
-  if num == 0:
-    print("There are no more green bottles hanging on the wall.")
-    break
-  elif how_many == num:
-    print(f"There will be {num} green bottles hanging on the wall.")
-    break
-  elif how_many != num:
-    print(f"No, try again")
+    how_many = int(input("How many green bottles will be hanging on the wall? "))
+
+    if how_many == num:
+        print(f"There will be {num} green bottles hanging on the wall.")
+        num -= 1
+        if num == 0:
+            print("There are no more green bottles hanging on the wall.")
+            break
+    else:
+        print("No, try again")
