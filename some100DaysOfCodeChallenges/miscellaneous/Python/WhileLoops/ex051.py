7c440a7d42694d4d854e0e36e355c8cf165372ad
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-29 21:48:23 -0300

    Update ex051.py

--- some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex051.py
+++ some100DaysOfCodeChallenges/miscellaneous/Python/WhileLoops/ex051.py
@@ -7,3 +7,12 @@ hanging on the wall?” If the user answers correctly, display the message “Th
 message “No, try again” until they get it right. When the number of green bottles gets
 down to 0, display the message “There are no more green bottles hanging on the wall”.
 '''
+
+num = 10
+
+print(f"There are {num} green bottles hanging on the wall, {num} green bottles hanging on the wall, and if 1 green bottle
+should accidentally fall")
+
+while True:
+  
+  
