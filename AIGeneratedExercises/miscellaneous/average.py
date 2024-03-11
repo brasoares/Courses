896b6f272bcb1761175a3f4d246411d83740eeb5
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-10 23:44:47 -0300

    Create average.py

---
+++ AIGeneratedExercises/miscellaneous/average.py
@@ -0,0 +1,8 @@
+# Define a function that calculates the average of a list of numbers
+def calculate_average(numbers):
+    pass  # Your code here
+
+# Test the function with a sample list of numbers
+sample_numbers = [5, 10, 15, 20, 25]
+average = calculate_average(sample_numbers)
+print("The average is:", average)
