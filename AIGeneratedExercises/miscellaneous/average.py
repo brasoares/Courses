462eeb4691fd9f697d79e4921278bb7f05c1caff
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-10 23:46:36 -0300

    Update average.py

--- AIGeneratedExercises/miscellaneous/average.py
+++ AIGeneratedExercises/miscellaneous/average.py
@@ -2,6 +2,7 @@
 def calculate_average(numbers):
     total = sum(numbers)
     average = total / len(numbers)
+    return average
 
 # Test the function with a sample list of numbers
 sample_numbers = [5, 10, 15, 20, 25]
