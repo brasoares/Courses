ffebc206545d21e88f7e688b083420203be45372
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-17 20:53:18 -0300

    Update rec_area.py

--- AIGeneratedExercises/Python/rec_area.py
+++ AIGeneratedExercises/Python/rec_area.py
@@ -4,6 +4,10 @@ l = 0
 w = 0
 area = 0
 // Prompt the user to enter the length of the rectangle
+
+l = float(input("Enter the rectangle's length: "
+
+
 // Read the length from the user input
 // Prompt the user to enter the width of the rectangle
 // Read the width from the user input
