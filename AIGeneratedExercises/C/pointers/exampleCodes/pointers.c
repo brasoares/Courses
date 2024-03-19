6b0a77b5e8684edcc9a611f90f4c10ead554f2a6
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-19 19:51:55 -0300

    Update pointers.c

--- AIGeneratedExercises/C/pointers/exampleCodes/pointers.c
+++ AIGeneratedExercises/C/pointers/exampleCodes/pointers.c
@@ -2,4 +2,11 @@ int main () {
   int num = 999;
   int *ptr; // Pointer declaration
   ptr = &num; // Pointer initialization
+
+  printf("Value of num: %d\n", num);
+  printf("Address of num: %p\n", (void *)&num);
+  printf("Value of num using pointer: %d\n", *ptr); // Dereferencing pointer
+  printf("Address stored in pointer: %p\n", (void *)ptr);
+
+  return 0;
 }
