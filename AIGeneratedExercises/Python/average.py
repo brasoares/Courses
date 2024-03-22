afd24864c97875d14bf54c31e52f9bc6b1593fbc
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-21 21:13:13 -0300

    Update average.py

--- AIGeneratedExercises/Python/average.py
+++ AIGeneratedExercises/Python/average.py
@@ -2,6 +2,6 @@ n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 n3 = int(input("Enter the third number: "))
 
-average = (n1 + n2 + n3) / 2
+average = (n1 + n2 + n3) / 3
 
 print(f"The average is: {average}"
