70358fde697f414f28182dac991d8001849c4dec
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-22 15:23:42 -0300

    Update palindrome.py

--- AIGeneratedExercises/Python/palindrome.py
+++ AIGeneratedExercises/Python/palindrome.py
@@ -4,3 +4,5 @@ checker = list(entrada)
 for letter in checker:
    validation.append(letter)
 
+if validation == validation:
+   print(f"A palavra informada é um palíndromo!/n{checker} = {validation}!")
