d23fbfc801ce2ccf4a65d57b98be20660f391408
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-25 21:32:44 -0300

    Update palindrome.c

--- AIGeneratedExercises/C/palindrome.c
+++ AIGeneratedExercises/C/palindrome.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () {
   char word[18];
   printf("Entre com a palavra: ");
   scanf("%s", word);
+
+  int length = strlen(word);
+  char reversed[length + 1];
 
   for(i = 0; str[i] != '\0'; i++) {
     reversed[i] = word[length - i - 1]
