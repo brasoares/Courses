8d4fc5d296768cddd617375b5935855141700535
Henoc Soares <henocsoares@hotmail.com.br>
2024-03-30 22:21:46 -0300

    Update Choice.c

--- AIGeneratedExercises/C/Choice.c
+++ AIGeneratedExercises/C/Choice.c
@@ -5,3 +5,13 @@ char userChoice;
 printf("Enter you choice: ");
 scanf("%s", &userChoice);
 
+switch(userChoice) {
+  case 'a':
+    printf("You chose option A.\n");
+    break;
+  case 'b':
+    printf("You chose option B.\n");
+    break;
+  default:
+    printf("Invalid choice.\n");
+}
