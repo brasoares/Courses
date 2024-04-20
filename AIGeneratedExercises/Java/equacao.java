895a20563b5104da2630b07e73a5cf07fda54ffd
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-19 23:19:39 -0300

    Update equacao.java
    Done! 999

--- AIGeneratedExercises/Java/equacao.java
+++ AIGeneratedExercises/Java/equacao.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 public class EncontrarIncognita {
   public static void main(Strin[] args) {
     Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite os valores de a, b e c na equação ax + b = c:");
+    System.out.print("a: ");
+    double a = scanner.nextDouble();
+
+    System.out.print("b: ");
+    double b = scanner.nextDouble();
+
+    System.out.print("c: ");
+    double c = scanner.nextDouble();
+
+    // Encontrando a incógnita x
+    double x = (c - b) / a;
+
+    System.out.println("O valor da incógnita x é: " + x);
   }
 }
