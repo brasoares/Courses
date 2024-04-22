bb396962939a0191c4951719cf1dc2c25be0260a
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-21 23:50:37 -0300

    Update stars.java

--- Java/Galactic/stars.java
+++ Java/Galactic/stars.java
@@ -3,9 +3,9 @@
 public class BrightestStar {
 
   // Ideally, properties should have access modifiers (private here)
-  public static void main (string[] args) {
+  
     private String name;
     private double radius; // in solar radii
     private double apparentMag; // perceived brightness
-  }
+  
 }
