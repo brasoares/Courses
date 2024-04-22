e3da1c5b2bf5b492fb898b198e0474a215021a47
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-21 23:49:58 -0300

    Update stars.java

--- Java/Galactic/stars.java
+++ Java/Galactic/stars.java
@@ -1,5 +1,6 @@
 /**Henoc S. Freire | 4/20/2024 at Carapicuíba | V 0.0 | stars.java */
 
+// Ideally, properties should have access modifiers (private here)
 public class BrightestStar {
   public static void main (string[] args) {
     private String name;
