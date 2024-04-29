676b6f57bd0ef54136028f953d485c040316e76f
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-28 23:57:17 -0300

    Update RememberingTheStructureDayOne.java

--- Java/Miscellaneous/RememberingTheStructureDayOne.java
+++ Java/Miscellaneous/RememberingTheStructureDayOne.java
@@ -1,5 +1,5 @@
-public class RememberingTheStructureDayOne() {
-  public void main(String[] args) {
+public class RememberingTheStructureDayOne{
+  public static void main(String[] args) {
     // Your code here!
     // The ideia here is to remember with no look to other tools, if something is missing, it will be commented next to the code and ahead of it!
   }
