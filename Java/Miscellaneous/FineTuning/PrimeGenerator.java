3fb9d7173d7e706221fac4f8947ab3a82d66f2c3
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-29 23:25:40 -0300

    Update PrimeGenerator.java

--- Java/Miscellaneous/FineTuning/PrimeGenerator.java
+++ Java/Miscellaneous/FineTuning/PrimeGenerator.java
@@ -1,1 +1,13 @@
+/******************************************************************************
 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class PrimeGenerator{
+    public static void main(String[] args) {
+            
+    }
+}
