bd34cca93210f9adec6c0200920982bf34d813cf
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-29 23:27:47 -0300

    Update PrimeGenerator.java

--- Java/Miscellaneous/FineTuning/PrimeGenerator.java
+++ Java/Miscellaneous/FineTuning/PrimeGenerator.java
@@ -6,8 +6,13 @@ Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
 
+// Sieve of Erastosthenes algorithm
+
 public class PrimeGenerator{
     public static void main(String[] args) {
-            
+        // Generates primes between 10 and 50
+        int m = 10;
+        int n = 50;
     }
 }
+
