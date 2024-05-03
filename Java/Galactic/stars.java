bf10b4b1f4b2eb4b97bc5ebe6573d6ca281b585b
Henoc Soares <henocsoares@hotmail.com.br>
2024-05-02 21:22:18 -0300

    Update stars.java

--- Java/Galactic/stars.java
+++ Java/Galactic/stars.java
@@ -30,50 +30,51 @@ public class BrightestStars {
   // Method to compare the size and brightness of two BrightestStars objects
   public static void compareStars(BrightestStars star1, BrightestStars star2) {
     // Compare size
-    if (star1.getRadiusSolarRadii() > star2.getRadiusSolarRadii()){
+    if (star1.getRadiusSolarRadii() > star2.getRadiusSolarRadii()) {
       System.out.println(star1.getName() + " is bigger than " + star2.getName();
-    } else if (star1.getRadiusSolarRadii() < star2.getRadiusSolarRadii()) {
-      System.out.println(star2.getName() + " is bigger than" + star1.getName());
-    } else {
-      System.out.println(star1.getName() + " and " + star2.getName() + " are the same size.");
-    }
-  }
-
-  public static void main(String[] args) {
-    // Create BrightestStar objects with static data
-    BrightestStars sirius = new BrightestStars("Sirius", 1.18, -1.46);
-    BrightestStars canopus = new BrightestStars("Canopus", 310, -0.74);
-    BrightestStars alphaCentauri = new BrightestStars("Alpha Centauri", 4.3, -0.01);
-    BrightestStars arcturus = new BrightestStars("Arcturus", 36, -0.05);
-    BrightestStars vega = new BrightestStars("Vega", 25, 0.03);
-    BrightestStars capella = new BrightestStars("Capella", 42, 0.08);
-
-    // Menu-driven system
-    Scanner scanner = new Scanner(System.in);
-    int choice;
-    do {
-      System.out.println("\nMenu:");
-      System.out.println("1. Compare two specific stars");
-      System.out.println("2. Compare randomly selected stars");
-      System.out.println("3. Exit");
-      System.out.print("Enter your choice: ");
-      choice = scanner.nextInt();
-
-      switch (choice) {
-      case 1:
-        compareSpecificStars(scanner, sirius, canopus, alphaCentauri, arcturus, vega, capella);
-        break;
-      case 2:
-        compareRandomStars(sirius, canopus, alphaCentauri, arcturus, vega, capella);
-        break;
-      case 3:
-        System.out.println("Exiting program. Goodybye!");
-        break;
-      default:
-        System.out.println("Invalid choice. Please enter a valid option next time.");
-
       }
-      while (choice != 3);
+      else if (star1.getRadiusSolarRadii() < star2.getRadiusSolarRadii()) {
+        System.out.println(star2.getName() + " is bigger than" + star1.getName());
+      } else {
+        System.out.println(star1.getName() + " and " + star2.getName() + " are the same size.");
+      }
+    }
+
+    public static void main(String[] args) {
+      // Create BrightestStar objects with static data
+      BrightestStars sirius = new BrightestStars("Sirius", 1.18, -1.46);
+      BrightestStars canopus = new BrightestStars("Canopus", 310, -0.74);
+      BrightestStars alphaCentauri = new BrightestStars("Alpha Centauri", 4.3, -0.01);
+      BrightestStars arcturus = new BrightestStars("Arcturus", 36, -0.05);
+      BrightestStars vega = new BrightestStars("Vega", 25, 0.03);
+      BrightestStars capella = new BrightestStars("Capella", 42, 0.08);
+
+      // Menu-driven system
+      Scanner scanner = new Scanner(System.in);
+      int choice;
+      do {
+        System.out.println("\nMenu:");
+        System.out.println("1. Compare two specific stars");
+        System.out.println("2. Compare randomly selected stars");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+        choice = scanner.nextInt();
+
+        switch (choice) {
+        case 1:
+          compareSpecificStars(scanner, sirius, canopus, alphaCentauri, arcturus, vega, capella);
+          break;
+        case 2:
+          compareRandomStars(sirius, canopus, alphaCentauri, arcturus, vega, capella);
+          break;
+        case 3:
+          System.out.println("Exiting program. Goodybye!");
+          break;
+        default:
+          System.out.println("Invalid choice. Please enter a valid option next time.");
+
+        }
+        while (choice != 3);
+      }
     }
   }
-}
