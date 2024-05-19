7354a005891b1a28af8408c1f2c3934072f53da4
Henoc Soares <henocsoares@hotmail.com.br>
2024-05-18 23:25:00 -0300

    Update app.js

--- JavaScript/Alura/EstruturasDeRepeticao/app.js
+++ JavaScript/Alura/EstruturasDeRepeticao/app.js
@@ -1,13 +1,11 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 9;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // Se o chute for menor ou igual ao número secreto:
 if (chute == numeroSecreto) {
-	alert(`Isso aí!Você descobriu o número secreto $ {
-				numeroSecreto
-			}, parabéns!`);
+  alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}, parabéns!`);
 } else {
-  alert('Você não acertou desta vez; tentar novamente!');
+  alert('Você não acertou desta vez; tente novamente!');
 }
