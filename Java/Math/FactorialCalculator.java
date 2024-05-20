2ce3fe6c5c5c5c75ee7c1541799a82ce829505cf
Henoc Soares <henocsoares@hotmail.com.br>
2024-05-19 23:49:10 -0300

    Update FactorialCalculator.java

--- Java/Math/FactorialCalculator.java
+++ Java/Math/FactorialCalculator.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class FactorialCalculator {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a positive integer: ");
+    int number = scanner.nextInt();
   } 
 }
