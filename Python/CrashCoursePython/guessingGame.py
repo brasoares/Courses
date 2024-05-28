8149d6e0425e609d13f7dfdba2e558b945dcf7fb
Henoc Soares <henocsoares@hotmail.com.br>
2024-05-27 22:17:33 -0300

    Update guessingGame.py

--- Python/CrashCoursePython/guessingGame.py
+++ Python/CrashCoursePython/guessingGame.py
@@ -1,12 +1,12 @@
 import sys
 guessn = 33
-userNum = 0
-while userNum < guessn or userNum > guessn:
-    userNum = int(input("Qual é o número? "))
-    if userNum == guessn:
+guess = 0
+while guess < guessn or guess > guessn:
+    guess = int(input("Qual é o número? "))
+    if guess == guessn:
         print("You're right! Your guess is correct! Congrats!")
         sys.exit()
-    elif userNum < guessn:
+    elif guess < guessn:
         print("Guess Higher")
     else:
         print("Guess Lower")
